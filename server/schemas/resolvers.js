eff62b03ef244da5cf86b105a674349af0096d67
ckyle121 <carolinekyle21@gmail.com>
2022-05-04 12:36:54 -0500

    create query resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,24 @@
 const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books');
+                
+                return userData;
+            }
+           throw new AuthenticationError('Not logged in'); 
+        }
+    },
+
+    Mutation: {
+
+    }
+}
+
+module.exports = resolvers;
