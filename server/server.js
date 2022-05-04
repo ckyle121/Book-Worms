79fdabbefae1a9ef38af9194e8881cb4e1568ac6
ckyle121 <carolinekyle21@gmail.com>
2022-05-04 16:17:12 -0500

    sever js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
-const path = require('path');
-
-const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
+
+const path = require('path');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const {typeDefs, resolvers} = require('./schemas');
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -14,11 +14,12 @@ const server = new ApolloServer({
 });
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
+//Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
