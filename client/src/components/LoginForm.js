4fd7589e66bb9f00379f3c939cb6aaabc86a91d9
ckyle121 <carolinekyle21@gmail.com>
2022-05-04 19:32:08 -0500

    update sign up for graph ql

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -29,10 +29,10 @@ const LoginForm = () => {
     }
 
     try {
-      const { data } = await loginUser({ variables: { ...userFormData } });
+      const { data } = await login({ variables: { ...userFormData } });
 
       Auth.login(data.login.token);
-      
+
     } catch (err) {
       console.error(err);
       setShowAlert(true);
--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -31,15 +31,10 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({ variables: {...userFormData} });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      Auth.login(data.addUser.token);
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
