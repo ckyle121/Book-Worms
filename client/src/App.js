541be1de3a59cae417ae52a81fcfb338619cfbae
ckyle121 <carolinekyle21@gmail.com>
2022-05-05 12:16:18 -0500

    fix token issue

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
@@ -14,8 +16,20 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.httpLink,
   cache: new InMemoryCache(),
 });
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,7 +21,7 @@ const resolvers = {
           console.log(args)
             const user = await User.create(args);
             const token = signToken(user);
-      
+            console.log(token);
             return { token, user };
         },
         login: async (parent, { email, password }) => {
