d772a171b02893aee2166a14387b1059f7db460e
ckyle121 <carolinekyle21@gmail.com>
2022-05-06 11:30:08 -0500

    update mutation for save books

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -42,7 +42,7 @@ const SavedBooks = () => {
     <>
       <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Viewing saved books!</h1>
+          <h1>Viewing Saved Books!</h1>
         </Container>
       </Jumbotron>
       <Container>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,6 +26,7 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
   mutation saveBook($input: savedBook) {
     saveBook(input: $input) {
+      _id
       username
       email
       savedBooks{
